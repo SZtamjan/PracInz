44ce508db9d93c437965c86fa169cfaa8ca406a8
SZtamjan <tamjan001@gmail.com>
2025-01-31 21:57:32 +0100

    Animations and AnimatorController

--- Assets/_Code/Scripts/GameManager.cs
+++ Assets/_Code/Scripts/GameManager.cs
@@ -1,43 +1,143 @@
 ﻿using System;
+using System.ComponentModel;
+using _Code.Scripts.Obstacles;
 using _Code.Scripts.Player;
+using _Code.Scripts.Points;
+using _Code.Scripts.RoadSystem;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace _Code.Scripts
 {
     public class GameManager : MonoBehaviour
     {
+        //Components
         private PlayerManager _playerManager;
+        private RoadManager _roadManager;
+        private ObstacleManager _obstacleManager;
+        
+        
+        private GameState _gameState;
+        private static readonly int TrFastRun = Animator.StringToHash("TrFastRun");
+        private static readonly int TrStop = Animator.StringToHash("TrStop");
+        private static readonly int TrRun = Animator.StringToHash("TrRun");
+        private static readonly int TrFly = Animator.StringToHash("TrFly");
+        private static readonly int State = Animator.StringToHash("GameState");
 
+        public GameState GameState
+        {
+            get => _gameState;
+            private set
+            {
+                if (!Enum.IsDefined(typeof(GameState), value))
+                    throw new InvalidEnumArgumentException(nameof(value), (int)value, typeof(GameState));
+                _gameState = value;
+                StateChanged();
+            }
+        }
+        
         private void Awake()
         {
             PrepareComponents();
         }
+
+        private void StateChanged()
+        {
+            _playerManager.playerAnimator.SetInteger(State,(int)GameState);
+            
+            switch (GameState)
+            {
+                case GameState.Stop:
+                    Stop();
+                    _playerManager.IAmRunning = false;
+                    break;
+                case GameState.Run:
+                    Run();
+                    _playerManager.IAmRunning = true;
+                    break;
+                case GameState.FastRun:
+                    FastRun();
+                    _playerManager.IAmRunning = true;
+                    break;
+                case GameState.Fly:
+                    Flight();
+                    _playerManager.IAmRunning = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void Stop()
+        {
+            //Time.timeScale = 0f;
+            _roadManager.roadSpeed = 0f;
+            _obstacleManager.obstacleSpeed = 0f;
+        }
         
+        private void Run()
+        {
+            Time.timeScale = 1f;
+            _roadManager.roadSpeed = _playerManager.PlayerSpeed;
+            _obstacleManager.obstacleSpeed = _playerManager.PlayerSpeed;
+        }
+
+        private void FastRun()
+        {
+            Time.timeScale = 1f;
+            _roadManager.roadSpeed = _playerManager.PlayerFastSpeed;
+            _obstacleManager.obstacleSpeed = _playerManager.PlayerFastSpeed;
+        }
+
+        private void Flight()
+        {
+            Time.timeScale = 1f;
+            _roadManager.roadSpeed = _playerManager.PlayerFlightSpeed;
+            _obstacleManager.obstacleSpeed = _playerManager.PlayerFlightSpeed;
+        }
+
+        #region Debug
+
+        [Button]
+        private void ChangeToStop()
+        {
+            GameState = GameState.Stop;
+        }
         
+        [Button]
+        private void ChangeToRun()
+        {
+            GameState = GameState.Run;
+        }
         
+        [Button]
+        private void ChangeToFastRun()
+        {
+            GameState = GameState.FastRun;
+        }
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        [Button]
+        private void ChangeToFlight()
+        {
+            GameState = GameState.Fly;
+        }
+
+        #endregion
+
         private void PrepareComponents()
         {
             _playerManager = PlayerManager.Instance;
+            _playerManager.PrepareComponents();
+            _roadManager = RoadManager.Instance;
+            _obstacleManager = ObstacleManager.Instance;
+            GameState = GameState.Run;
         }
     }
-    
-    
+    public enum GameState
+    {
+        Stop,
+        Run,
+        FastRun,
+        Fly,
+    }
 }
--- Assets/_Code/Scripts/Player/PlayerManager.cs
+++ Assets/_Code/Scripts/Player/PlayerManager.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.ComponentModel;
-using _Code.Scripts.Obstacles;
-using _Code.Scripts.Points;
-using _Code.Scripts.RoadSystem;
+﻿using _Code.Scripts.Points;
 using _Code.Scripts.Singleton;
-using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _Code.Scripts.Player
 {
@@ -15,155 +11,52 @@ namespace _Code.Scripts.Player
         [SerializeField] private float playerFastSpeed;
         [SerializeField] private float playerFlightSpeed;
 
-        private float PlayerSpeed
+        [HideInInspector] public PointCounter pointCounter;
+        [HideInInspector] public Animator playerAnimator;
+        
+        public float PlayerSpeed
         {
             get
             {
-                _pointCounter.currentSpeed = playerSpeed;
+                pointCounter.currentSpeed = playerSpeed;
                 return playerSpeed;
             }
         }
 
-        private float PlayerFastSpeed
+        public float PlayerFastSpeed
         {
             get
             {
-                _pointCounter.currentSpeed = playerFastSpeed;
+                pointCounter.currentSpeed = playerFastSpeed;
                 return playerFastSpeed;
             }
         }
 
-        private float PlayerFlightSpeed
+        public float PlayerFlightSpeed
         {
             get
             {
-                _pointCounter.currentSpeed = playerFlightSpeed;
+                pointCounter.currentSpeed = playerFlightSpeed;
                 return playerFlightSpeed;
             }
         }
         
-        private bool _iAmRunning;
+        [HideInInspector] public bool _iAmRunning;
 
-        private bool IAmRunning
+        public bool IAmRunning
         {
             get => _iAmRunning;
             set
             {
                 _iAmRunning = value;
-                _pointCounter.IAmRunning = value;
-            }
-        }
-        
-        //Components
-        private RoadManager _roadManager;
-        private ObstacleManager _obstacleManager;
-        private PointCounter _pointCounter;
-        
-        private PlayersState _playersState;
-
-        public PlayersState playersState
-        {
-            get => _playersState;
-            private set
-            {
-                if (!Enum.IsDefined(typeof(PlayersState), value))
-                    throw new InvalidEnumArgumentException(nameof(value), (int)value, typeof(PlayersState));
-                _playersState = value;
-                StateChanged();
+                pointCounter.IAmRunning = value;
             }
         }
 
-        private void Awake()
+        public void PrepareComponents()
         {
-            _pointCounter = PointCounter.Instance;
-            _roadManager = RoadManager.Instance;
-            _obstacleManager = ObstacleManager.Instance;
-            playersState = PlayersState.Run;
+            playerAnimator = GetComponent<Animator>();
+            pointCounter = PointCounter.Instance;
         }
-
-        private void StateChanged()
-        {
-            switch (playersState)
-            {
-                case PlayersState.Stop:
-                    Stop();
-                    IAmRunning = false;
-                    break;
-                case PlayersState.Run:
-                    Run();
-                    IAmRunning = true;
-                    break;
-                case PlayersState.FastRun:
-                    FastRun();
-                    IAmRunning = true;
-                    break;
-                case PlayersState.Fly:
-                    Flight();
-                    IAmRunning = true;
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        private void Stop()
-        {
-            _roadManager.roadSpeed = 0f;
-            _obstacleManager.obstacleSpeed = 0f;
-        }
-        
-        private void Run()
-        {
-            _roadManager.roadSpeed = PlayerSpeed;
-            _obstacleManager.obstacleSpeed = PlayerSpeed;
-        }
-
-        private void FastRun()
-        {
-            _roadManager.roadSpeed = PlayerFastSpeed;
-            _obstacleManager.obstacleSpeed = PlayerFastSpeed;
-        }
-
-        private void Flight()
-        {
-            _roadManager.roadSpeed = PlayerFlightSpeed;
-            _obstacleManager.obstacleSpeed = PlayerFlightSpeed;
-        }
-
-        #region Debug
-
-        [Button]
-        private void ChangeToStop()
-        {
-            playersState = PlayersState.Stop;
-        }
-        
-        [Button]
-        private void ChangeToRun()
-        {
-            playersState = PlayersState.Run;
-        }
-        
-        [Button]
-        private void ChangeToFastRun()
-        {
-            playersState = PlayersState.FastRun;
-        }
-        
-        [Button]
-        private void ChangeToFlight()
-        {
-            playersState = PlayersState.Fly;
-        }
-
-        #endregion
-    }
-
-    public enum PlayersState
-    {
-        Stop,
-        Run,
-        FastRun,
-        Fly,
     }
 }
