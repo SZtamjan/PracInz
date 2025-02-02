24ee08c0c492a0d7e68273484d2e3b91c6b1fd9c
SZtamjan <tamjan001@gmail.com>
2025-02-02 21:07:08 +0100

    fixed ui game over now appears, fixed uicam seeing road, fixed ingame ui is now scalable

--- Assets/_Code/Scripts/Obstacles/KillerObstacle.cs
+++ Assets/_Code/Scripts/Obstacles/KillerObstacle.cs
@@ -1,4 +1,5 @@
 ﻿using _Code.Scripts.Player;
+using _Code.Scripts.UIScripts;
 using UnityEngine;
 
 namespace _Code.Scripts.Obstacles
@@ -10,6 +11,7 @@ namespace _Code.Scripts.Obstacles
             if (other.gameObject.TryGetComponent(out PlayerManager playerManager))
             {
                 if(GameManager.Instance.GameState == GameState.Fly) return;
+                UIController.Instance.InGameUIState = InGameUIStates.GameOverIU;
                 GameManager.Instance.GameState = GameState.Stop;
             }
         }
