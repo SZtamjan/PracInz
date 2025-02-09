e7c24a8bbea897ab3b5a3f55a17704786b113e0a
SZtamjan <tamjan001@gmail.com>
2025-02-09 16:05:38 +0100

    correct one

---
+++ Assets/_Code/Scripts/ExampleStateMachineScript.cs
@@ -0,0 +1,44 @@
+﻿namespace _Code.Scripts
+{
+    public class ExampleStateMachineScript
+    {
+        public enum GameState
+        {
+            Menu,
+            Gameplay,
+            Pause,
+            GameOver
+        }
+
+        private GameState _currentState;
+
+        public GameState CurrentState
+        {
+            get => _currentState;
+            set
+            {
+                _currentState = value;
+                UpdateState();
+            }
+        }
+
+        void UpdateState()
+        {
+            switch (CurrentState)
+            {
+                case GameState.Menu:
+                    //HandleMenu();
+                    break;
+                case GameState.Gameplay:
+                    //HandleGameplay();
+                    break;
+                case GameState.Pause:
+                    //HandlePause();
+                    break;
+                case GameState.GameOver:
+                    //HandleGameOver();
+                    break;
+            }
+        }
+    }
+}
